c32a055a5912f3ccbfd0017f71611af35ebf912c
V-Dev_P <aravis2931@yahoo.gr>
2025-02-09 21:37:33 +0200

    Changes to be committed:   modified:   backend/middleware/protectRoute.js

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { User } from '../models/user.model.js';
+import { User } from '../models/User.model.js';
 import { ENV_VARS } from '../config/envVars.js';
 
 //verification of jwt token and user authentication of user id of cookie
